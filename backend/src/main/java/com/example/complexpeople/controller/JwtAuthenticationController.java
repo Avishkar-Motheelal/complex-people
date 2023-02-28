cabe153cdda51968dbd410a31597055eb63142d0
Avishkar Motheelal <avishkar.motheelal11@gmail.com>
2023-02-28 02:59:42 +0200

    Backend can receive token and get payload from frontend

--- backend/src/main/java/com/example/complexpeople/controller/JwtAuthenticationController.java
+++ backend/src/main/java/com/example/complexpeople/controller/JwtAuthenticationController.java
@@ -1,11 +1,16 @@
 package com.example.complexpeople.controller;
 
-import java.util.Objects;
-
 import com.example.complexpeople.security.jwt.JwtRequest;
 import com.example.complexpeople.security.jwt.JwtResponse;
 import com.example.complexpeople.security.jwt.JwtTokenUtil;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,11 +18,12 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,6 +33,8 @@ public class JwtAuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenUtil jwtTokenUtil;
     private final UserDetailsService userDetailsService;
+    @Value("${app.googleClientId}")
+    private String clientId;
 
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
@@ -53,5 +61,43 @@ public class JwtAuthenticationController {
         } catch (BadCredentialsException e) {
             throw new Exception("INVALID_CREDENTIALS", e);
         }
+    }
+
+
+    @PostMapping("/login/oauth2/code/google")
+    public void tokenThing(@RequestBody String token) throws GeneralSecurityException, IOException {
+
+        String idTokenString = token.replaceAll("\"", "");
+        System.out.println("Token String: " + idTokenString);
+        HttpTransport transport = new NetHttpTransport();
+        JsonFactory jsonFactory = new GsonFactory();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+            .setAudience(Collections.singletonList(clientId))
+            .build();
+
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = payload.getEmailVerified();
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+            System.out.println(payload.toPrettyString());
+            // Use or store profile information
+            // ...
+
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+//        return "abc";
     }
 }
