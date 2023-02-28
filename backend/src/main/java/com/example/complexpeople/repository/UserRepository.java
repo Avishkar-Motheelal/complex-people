5f5c513e9c331cfece0bbd023dbc7bb14fec722a
Avishkar Motheelal <avishkar.motheelal11@gmail.com>
2023-02-28 02:59:42 +0200

    Backend can receive token and get payload from frontend

--- backend/src/main/java/com/example/complexpeople/repository/UserRepository.java
+++ backend/src/main/java/com/example/complexpeople/repository/UserRepository.java
@@ -2,19 +2,18 @@ package com.example.complexpeople.repository;
 
 import com.example.complexpeople.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query(value = """
-            SELECT u.* FROM Users u
-            INNER JOIN People p on u.PeopleId = p.PeopleId
-            INNER JOIN ContactDetails cd on p.ContactDetailsId = cd.ContactDetailsId
-            WHERE cd.EmailAddress = ?1
-        """, nativeQuery = true)
+    //    @Query(value = """
+//            SELECT u.* FROM Users u
+//            INNER JOIN People p on u.PeopleId = p.PeopleId
+//            INNER JOIN ContactDetails cd on p.ContactDetailsId = cd.ContactDetailsId
+//            WHERE cd.EmailAddress = ?1
+//        """, nativeQuery = true)
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUserId(long id);
