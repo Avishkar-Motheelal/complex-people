1fccc0d86fe826baa3a81f73ecef66d22a5c639e
bbdnet2865 <samkele@bbd.co.za>
2023-03-02 07:51:02 +0200

    changes

--- frontend/complex-people/src/app/components/maintenance-form/maintenance-form.component.ts
+++ frontend/complex-people/src/app/components/maintenance-form/maintenance-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {IssuesModel} from "../../models/issues_model";
 import {TheserviceService} from "../../services/theservice.service";
+import {IssuesService} from "../../services/issues.service";
 
 @Component({
   selector: 'app-maintenance-form',
@@ -16,7 +17,7 @@ export class MaintenanceFormComponent {
     this.formHidden = true;
   }
 
-  constructor(private sharedService: TheserviceService) {
+  constructor(private sharedService: TheserviceService, private issueService: IssuesService) {
     this.issue$ = new IssuesModel("", 0, "", "", new Date(), "", "");
     this.sharedService.myMethod$.subscribe((data) => {
       this.formHidden = data;
